067a61b5ae7813cd6b3c0e2d3379d2babc6041ec
jyonleon7 <jyonleon7@gmail.com>
2023-04-04 07:44:11 +0900

    setup graphql

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,16 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      // コードファーストで開発を進める場合に、nest で自動生成されるgraphqlのスキーマの出力先を指定
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
 })
 export class AppModule {}
