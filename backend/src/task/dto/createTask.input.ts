af62175e09cd2a99b0c01d10860bfe4752a48966
jyonleon7 <jyonleon7@gmail.com>
2023-04-04 22:37:49 +0900

    入力値のバリデーションの追加

--- backend/src/task/dto/createTask.input.ts
+++ backend/src/task/dto/createTask.input.ts
@@ -1,13 +1,18 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
 
 @InputType()
 export class CreateTaskInput {
   @Field()
+  @IsNotEmpty()
+  @IsString()
   name: string;
 
   @Field()
+  @IsDateString()
+  @IsNotEmpty()
   dueDate: string;
 
-  @Field()
+  @Field({ nullable: true })
   description?: string;
 }
